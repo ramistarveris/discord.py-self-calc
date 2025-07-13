a18aa872fdfc19423456879e584c34baadaf9310
nkz.tm <167677250+ramistarveris@users.noreply.github.com>
2025-07-14 02:13:47 +0900

    Update python.py

--- python.py
+++ python.py
@@ -1,9 +1,11 @@
 import os
+import ast
+import operator
 from dotenv import load_dotenv
 import discord
 
 # 設定:
-prefix = "!"
+prefix = ";"
 
 load_dotenv()
 token = os.getenv("TOKEN")
@@ -18,6 +20,27 @@ replace_table = str.maketrans({
     "！": "!", "　": " ",
 })
 
+allowed_ops = { ast.Add: operator.add, ast.Sub: operator.sub, ast.Mult: operator.mul, ast.Div: operator.truediv, ast.Pow: operator.pow, ast.USub: operator.neg, ast.UAdd: operator.pos, }
+
+def safe_eval(expr: str):
+    node = ast.parse(expr, mode='eval')
+
+    def _eval(n):
+        if isinstance(n, ast.Expression):
+            return _eval(n.body)
+        elif isinstance(n, ast.Constant):
+            if isinstance(n.value, (int, float)):
+                return n.value
+        elif isinstance(n, ast.BinOp):
+            if type(n.op) in allowed_ops:
+                return allowed_ops[type(n.op)](_eval(n.left), _eval(n.right))
+        elif isinstance(n, ast.UnaryOp):
+            if type(n.op) in allowed_ops:
+                return allowed_ops[type(n.op)](_eval(n.operand))
+        raise ValueError("Invalid expression")
+
+    return _eval(node)
+
 @client.event
 async def on_ready():
     print(f"logged as {client.user} ({client.user.id})")
@@ -30,9 +53,9 @@ async def on_message(message):
         if not expression:
             return
         try:
-            result = eval(expression, {"__builtins__": None}, {})
+            result = safe_eval(expression)
             await message.channel.send(str(result))
-        except:
-            pass
+        except Exception as e:
+            await message.channel.send(e)
 
 client.run(token)
